c376f2562a43482b58eedf69676d1108d266bcab
HaoZhang <zhanghao997@qq.com>
2023-05-05 08:38:38 +0800

    use constexpr

--- util.h
+++ util.h
@@ -2,28 +2,28 @@
 
 #include <cassert>
 #include <cstdint>
-#define STAT_GRAIN 2
-#define STAT_MAX 10000
-#define STAT_LEN STAT_MAX / STAT_GRAIN
 #define RUN_SECONDS 30
-
 #define TABLE_NAME "films"
 
-class LantencyStat {
+// statistics of latency in milliseconds
+class LatencyStat {
+  static constexpr uint32_t bucket_size = 5;
+  static constexpr uint32_t bucket_num = 1000;
+
 public:
   void Record(uint32_t latency) {
     counter_++;
     latency_sum_ += latency;
-    if (latency >= STAT_MAX) {
-      latency_stat_[STAT_LEN - 1]++;
+    if (latency >= bucket_size * bucket_num) {
+      latency_stat_[bucket_num - 1]++;
     } else {
-      latency_stat_[latency / STAT_GRAIN]++;
+      latency_stat_[latency / bucket_size]++;
     }
   };
   uint32_t LatencyPercentiles(double p) {
     assert(p <= 1);
     uint32_t left = counter_;
-    int32_t i = STAT_LEN - 1;
+    int32_t i = bucket_num - 1;
     for (; i >= 0; --i) {
       if (double(left - latency_stat_[i]) / double(counter_) >= p) {
         left -= latency_stat_[i];
@@ -31,7 +31,7 @@ public:
         break;
       }
     }
-    return (i + 1) * STAT_GRAIN;
+    return (i + 1) * bucket_size;
   }
   uint32_t AverageLatency() {
     if (counter_ == 0) {
@@ -46,10 +46,10 @@ public:
     }
     assert(count == counter_);
   }
-  LantencyStat &operator+=(const LantencyStat &rhs) {
+  LatencyStat &operator+=(const LatencyStat &rhs) {
     this->counter_ += rhs.counter_;
     this->latency_sum_ += rhs.latency_sum_;
-    for (uint32_t i = 0; i < STAT_LEN; ++i) {
+    for (uint32_t i = 0; i < bucket_num; ++i) {
       this->latency_stat_[i] += rhs.latency_stat_[i];
     }
     return *this;
@@ -57,7 +57,7 @@ public:
   uint32_t Count() { return counter_; }
 
 private:
-  uint32_t latency_stat_[STAT_LEN] = {}; // ms
-  uint32_t latency_sum_{0};              // ms
+  uint32_t latency_stat_[bucket_num] = {};
+  uint32_t latency_sum_{0};
   uint32_t counter_{0};
 };
